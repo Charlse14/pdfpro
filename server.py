0342d836df38eee8bc1d1543dfb0a97066311d64
chetan goyal <goyalchetan078@gmail.com>
2020-08-10 14:09:58 +0530

    changing names of files and adding code to server file

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, send_from_directory
 from PIL import Image 
+import os
 
 app = Flask(__name__)
 
@@ -10,20 +11,52 @@ def hello_world():
     return render_template('index.html')
 
 
+
+
+
 # it is rendering different page based on the click   
 
 @app.route('/img', methods=['POST', 'GET'])
+
 def image_to_pdf():
 
     download = False
+
     if request.method == 'POST':
 
         file = request.files['myfile']
-        img = Image.open(file)
-        img.save("static\converted_pdfs\converted.pdf")
+        img = Image.open(file).convert('RGB')
+
+        path = os.getcwd()
+        path_list = ['static', 'client', 'pdfs']
+
+        for i in path_list:
+            path = os.path.join(path, i)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        joined_path = os.path.join(path, 'converted.pdf')
+        a = img.save(joined_path)
+
         download = True
 
     return render_template('image.html', downloadable=download)
+
+
+@app.route('/download')
+def download_converted_pdf():
+
+    path = os.getcwd()
+    path_list = ['static', 'client', 'pdfs']
+
+    print('hit path')
+
+    for i in path_list:
+        path = os.path.join(path, i)
+
+    return send_from_directory(str(path), filename='converted.pdf', as_attachment=True)
+
 
 
 
